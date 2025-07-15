86292c4ba2dffdf79fc1611cc4e6f905c6c62a8c
Guilherme Martins <guilhermemm.dev@gmail.com>
2025-07-15 11:55:04 -0300

    feat: add Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
 
   return (
     <>
-      <p>test</p>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <h1 className="text-3xl font-bold underline"> test tailwind </h1>
+        <Button>ShadcnButton</Button>
+      </div>
     </>
   )
 }
